e982f1f12600b92571c1b64c341f48768c5cbd30
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2019-06-28 18:58:04 +0300

    wip

--- Samples/Example01/Example01Panel.cs
+++ Samples/Example01/Example01Panel.cs
@@ -6,103 +6,29 @@ using Unity.UIWidgets.foundation;
 using System.Collections.Generic;
 namespace KABBOUCHI.UIWidgets.Provider.Examples
 {
-    public class ModelTest : ChangeNotifier
-    {
-        public ModelTest()
-        {
-            Debug.Log("new ModelTest");
-        }
-        private int _number;
-        public int Number
-        {
-            get
-            {
-                return _number;
-            }
-            set
-            {
-                _number = value;
-                this.notifyListeners();
-            }
-        }
-
-
-    }
-
     public class Example01Panel : UIWidgetsPanel
     {
 
-        ModelTest model = new ModelTest();
-
-        public static Example01Panel Instance;
-
-        protected override void Update()
-        {
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Inc();
-            }
-
-            base.Update();
-        }
-
-        public void Inc()
-        {
-            using (window.getScope())
-            {
-                model.Number++;
-            }
-        }
-
         protected override Widget createWidget()
         {
-            Instance = this;
-            // return new ProviderTest();
-            return new ChangeNotifierProviderTest();
-
+            return new ProviderTest();
         }
-
-        class ProviderTest : StatelessWidget
-        {
-            public override Widget build(BuildContext context)
-            {
-                return new MaterialApp(
-                 home: new Provider<int>(
-                        value: 42,
-                        child: new Consumer<int>(
-                            builder: (ctx, value) => new Text(value.ToString())
-                        )
-                    )
-             );
-            }
-        }
-        class ChangeNotifierProviderTest : StatelessWidget
-        {
-
-            public override Widget build(BuildContext context)
-            {
-                return new MaterialApp(
-            home: new Column(
-            children: new List<Widget>{
-                    new RaisedButton(
-                        child : new Text("Inc"),
-                        onPressed: () => {
-                            Example01Panel.Instance.model.Number++;
-                        }
-                    ),
-                    new ChangeNotifierProvider<ModelTest>(
-                        notifier :Example01Panel.Instance.model,
-                        builder :(ctx,notifier) => new Consumer<ModelTest>(
-                            builder  : (c,m) => new Text(m.Number.ToString())
-                        )
-                    )
-                }
-        )
-                );
-            }
-        }
-
     }
 
+    class ProviderTest : StatelessWidget
+    {
+        public override Widget build(BuildContext context)
+        {
+            return new MaterialApp(
+             home: new Provider<int>(
+                    value: 42,
+                    child: new Center(
+                        child: new Consumer<int>(
+                        builder: (ctx, value) => new Text(value.ToString())
+                      )
+                )
+            )
+         );
+        }
+    }
 }
---
+++ Samples/Example02/Example02Panel.cs
@@ -0,0 +1,92 @@
+﻿using Unity.UIWidgets.engine;
+using Unity.UIWidgets.widgets;
+using Unity.UIWidgets.material;
+using UnityEngine;
+using Unity.UIWidgets.foundation;
+using System.Collections.Generic;
+namespace KABBOUCHI.UIWidgets.Provider.Examples
+{
+    public class ModelTest : ChangeNotifier
+    {
+        public ModelTest()
+        {
+            Debug.Log("new ModelTest");
+        }
+        private int _number;
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+                this.notifyListeners();
+            }
+        }
+
+
+    }
+
+    public class Example02Panel : UIWidgetsPanel
+    {
+
+        ModelTest model = new ModelTest();
+
+        public static Example02Panel Instance;
+
+        protected override void Update()
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Inc();
+            }
+
+            base.Update();
+        }
+
+        public void Inc()
+        {
+            using (window.getScope())
+            {
+                model.Number++;
+            }
+        }
+
+        protected override Widget createWidget()
+        {
+            Instance = this;
+            return new ChangeNotifierProviderTest();
+
+        }
+        class ChangeNotifierProviderTest : StatelessWidget
+        {
+
+            public override Widget build(BuildContext context)
+            {
+                return new MaterialApp(
+            home: new Column(
+            children: new List<Widget>{
+                    new RaisedButton(
+                        child : new Text("Inc"),
+                        onPressed: () => {
+                            Example02Panel.Instance.model.Number++;
+                        }
+                    ),
+                    new ChangeNotifierProvider<ModelTest>(
+                        notifier :Example02Panel.Instance.model,
+                        builder :(ctx,notifier) => new Consumer<ModelTest>(
+                            builder  : (c,m) => new Text(m.Number.ToString())
+                        )
+                    )
+                }
+        )
+                );
+            }
+        }
+
+    }
+
+}
