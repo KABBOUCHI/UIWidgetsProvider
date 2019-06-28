5579b82aee1377c38627f611c4376ee38be245a5
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2019-06-28 17:22:56 +0300

    wip

---
+++ Runtime/widget_change_notifier_provider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.widgets;
+using UnityEngine;
+
+namespace KABBOUCHI.UIWidgets.Provider
+{
+
+    public class ChangeNotifierProvider<T> : StatefulWidget where T : ChangeNotifier
+    {
+        public readonly T notifier;
+        public readonly Func<BuildContext, T, Widget> builder;
+        public readonly Widget child;
+
+        public ChangeNotifierProvider(T notifier, Func<BuildContext, T, Widget> builder, Key key = null) : base(key)
+        {
+            this.notifier = notifier;
+            this.builder = builder;
+        }
+
+        public override State createState()
+        {
+            return new _ChangeNotifierProviderState<T>();
+        }
+    }
+
+    class _ChangeNotifierProviderState<T> : State<ChangeNotifierProvider<T>> where T : ChangeNotifier
+    {
+        public override void initState()
+        {
+            base.initState();
+
+            widget.notifier.addListener(rebuild);
+        }
+
+        public override void dispose()
+        {
+            base.dispose();
+
+            widget.notifier.removeListener(rebuild);
+        }
+
+        void rebuild()
+        {
+            setState();
+        }
+
+
+        public override Widget build(BuildContext context)
+        {
+            return new Provider<T>(
+                value: widget.notifier,
+                child: widget.builder(context, widget.notifier)
+            );
+        }
+    }
+
+}
---
+++ Runtime/widget_consumer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.widgets;
+
+namespace KABBOUCHI.UIWidgets.Provider
+{
+    public class Consumer<T> : StatelessWidget
+    {
+        Func<BuildContext, T, Widget> builder;
+
+        public Consumer(Func<BuildContext, T, Widget> builder, Key key = null) : base(key)
+        {
+            this.builder = builder;
+        }
+
+        public override Widget build(BuildContext context)
+        {
+            return builder(context, Provider.of<T>(context));
+        }
+    }
+
+    public class Consumer<T1, T2> : StatelessWidget
+    {
+        Func<BuildContext, T1, T2, Widget> builder;
+
+        public Consumer(Func<BuildContext, T1, T2, Widget> builder, Key key = null) : base(key)
+        {
+            this.builder = builder;
+        }
+
+        public override Widget build(BuildContext context)
+        {
+            return builder(context, Provider.of<T1>(context), Provider.of<T2>(context));
+        }
+    }
+
+}
